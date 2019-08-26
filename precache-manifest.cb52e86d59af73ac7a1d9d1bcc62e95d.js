1c7020e9655b18d70f1a0bf279a320cb55e150ed
Bobby Schade <bschade.18@gmail.com>
2019-08-26 08:34:51 -0500

    Updates

---
+++ precache-manifest.cb52e86d59af73ac7a1d9d1bcc62e95d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f770cd948cef631d4e935a5122404471",
+    "url": "/wiki-viewer-v2.0/index.html"
+  },
+  {
+    "revision": "ecf9de5dfa3349fdb5b1",
+    "url": "/wiki-viewer-v2.0/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "11431a800bfbbb6f5450",
+    "url": "/wiki-viewer-v2.0/static/js/2.c0e5c369.chunk.js"
+  },
+  {
+    "revision": "ecf9de5dfa3349fdb5b1",
+    "url": "/wiki-viewer-v2.0/static/js/main.1fa984de.chunk.js"
+  },
+  {
+    "revision": "88ccc67b91a9b3c6d79b",
+    "url": "/wiki-viewer-v2.0/static/js/runtime~main.35b9d659.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wiki-viewer-v2.0/static/media/logo.5d5d9eef.svg"
+  }
+]);
